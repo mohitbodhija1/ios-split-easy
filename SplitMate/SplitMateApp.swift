0380e0cf5db78697c325bc9f9b8780bc60232a5b
Mohit <mbodhija80@gmail.com>
2026-04-18 18:48:48 +0530

    Initial Commit

---
+++ SplitMate/SplitMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitMateApp.swift
+//  SplitMate
+//
+//  Created by Mohit Bodhija on 18/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SplitMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
